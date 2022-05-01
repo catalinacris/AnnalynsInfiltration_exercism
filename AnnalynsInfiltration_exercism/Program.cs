74fb2735d50341f326bd9eedcaafe25d4770a447
catalinacris <catalinacucos14@yahoo.com>
2022-05-01 05:57:24 +0200

    Add project files.

---
+++ AnnalynsInfiltration_exercism/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AnnalynsInfiltration_exercism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestLogic.CanFastAttack(true);
+            QuestLogic.CanSpy(false, true, false);
+            QuestLogic.CanSignalPrisoner(false, true);
+            QuestLogic.CanFreePrisoner(false, true, false, false);
+        }
+
+    }
+}
